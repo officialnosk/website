971d47a98d2604384756cb2475c78da3644dea3c
FancyCodeMaster <fancycodemaster@gmail.com>
2022-07-04 21:08:45 +0545

    just started footer's work

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footerContainer'>
+        <div className='footerLeftContainer'></div>
+        <div className='footerRightContainer'></div>
+    </footer>  )
+}
+
+export default Footer;
